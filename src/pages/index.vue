e236c7c789c3df9cf1b37289d56f7a6ae145240c
cheng <cheng@hicling.com>
2018-03-02 15:54:14 +0800

    no message

---
+++ src/pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="indexPage">
+		<v-chooser :selections="fruitList"></v-chooser>
+	</div>
+
+</template>
+
+<script>
+import VChooser from '../components/base/chooser/chooser'	
+export default {
+	components:{
+		VChooser
+	},
+	data(){
+      return {
+      	fruitList:[{
+      		label:'苹果',
+      		value:0
+      	},
+      	{
+      		label:'香蕉',
+      		value:1
+      	},
+      	{
+      		label:'西瓜',
+      		value:2
+      	},
+      	]
+      }
+	},
+	methods:{
+
+	},
+	mounted(){
+
+	}
+}
+
+</script>
+
+<style>
+.indexPage	{
+	height: 460px;
+}
+
+
+</style>
